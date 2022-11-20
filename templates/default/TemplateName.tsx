49daf9ee46ef79b75c62632c05d1cb26bbff9091
romarios1987 <romios047@gmail.com>
2022-11-20 11:20:38 +0200

    initial project

---
+++ templates/default/TemplateName.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './TemplateName.module.scss'
+
+interface TemplateNameProps {}
+
+const TemplateName = ({}: TemplateNameProps) => (
+	<div className={styles.TemplateName}>TemplateName Component</div>
+)
+
+export default TemplateName
